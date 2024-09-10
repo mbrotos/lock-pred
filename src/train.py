d0c61fc1ecea9b641fba44f0a62db9968d3d8007
Adam Sorrenti <adamsorrenti8@gmail.com>
2024-09-09 21:00:58 -0400

    Add logger and results exporting

--- src/train.py
+++ src/train.py
@@ -4,9 +4,13 @@ import argparse
 import json
 import keras
 import pandas as pd
+import os
+import datetime
+import uuid
 
 from datapipeline import load_data, create_sequences, prepare_datasets
 from model import build_lstm_model, build_transformer_model
+from utils import setup_logger
 
 def parse_args(args=None):
     parser = argparse.ArgumentParser(description="Train a model")
@@ -22,6 +26,7 @@ def parse_args(args=None):
     parser.add_argument("--vocab_size", type=int, default=900, help="Vocabulary size")
     parser.add_argument("--tokenization", type=str, default="char", help="Tokenization")
     parser.add_argument("--patience", type=int, default=5, help="Patience for early stopping")
+    parser.add_argument("--results_dir", type=str, default="results", help="Directory to save results")
 
     parser.add_argument("--shuffle", action="store_true", default=False, help="Shuffle data. Not recommended since we want to preserve sequence order.")
     parser.add_argument("--add_start_end_tokens", action="store_true", default=False, help="Add start and end tokens")
@@ -33,18 +38,18 @@ def parse_args(args=None):
 def evaluate_model(model, x_test, y_test, target_tokenizer, source_tokenizer):
     # Evaluate the model on the test dataset
     loss, accuracy = model.evaluate(x_test, y_test)
-    print(f"Per-output Test Accuracy: {accuracy * 100:.2f}%")
+    log.info(f"Per-output Test Accuracy: {accuracy * 100:.2f}%")
     
-    # Predict on a five example
-    print("Predicting on a five example")
-    for i in range(5):
+    # Predict on a few examples
+    log.info("Predicting on a few examples")
+    for i in range(15):
         # print("Input:", x_test[i])
-        print("Input text:", source_tokenizer.sequences_to_texts([x_test[i]]))
+        log.info(f"Input text: {source_tokenizer.sequences_to_texts([x_test[i]])}")
         # print("Expected output:", y_test[i])
-        print("Expected output text:", target_tokenizer.sequences_to_texts([np.argmax(y_test[i], axis=-1)]))
+        log.info(f"Expected output text: {target_tokenizer.sequences_to_texts([np.argmax(y_test[i], axis=-1)])}")
         output = model.predict(x_test[np.newaxis, i])
         # print("Predicted output:", output)
-        print("Predicted output text:", target_tokenizer.sequences_to_texts([np.argmax(output, axis=-1)[0]]))
+        log.info(f"Predicted output text: {target_tokenizer.sequences_to_texts([np.argmax(output, axis=-1)[0]])}")
     
     # calculate the accuracy using batching
     preds_all = model.predict(x_test)
@@ -54,13 +59,19 @@ def evaluate_model(model, x_test, y_test, target_tokenizer, source_tokenizer):
     for i in range(len(x_test)):
         if np.all(y_test_all[i] == preds_all[i]):
             count += 1
-    print(f"Actual Test Accuracy (n={len(x_test)}): {count/len(x_test) * 100:.2f}%")
+    log.info(f"Actual Test Accuracy (n={len(x_test)}): {count/len(x_test) * 100:.2f}%")
 
-def main():
-    args = parse_args()
+    results = {
+        "loss": loss,
+        "accuracy_per_output": accuracy,
+        "actual_test_accuracy": count/len(x_test),
+    }
+    return results
+
+def main(args):
     # Print args dict with indent
-    print(json.dumps(args.__dict__, indent=4))
-
+    log.info(f"Arguments:\n{json.dumps(args.__dict__, indent=4)}")
+    
     # TODO: Add checks for args given buisness logic
 
     # Load data
@@ -124,7 +135,7 @@ def main():
                   metrics=["accuracy"])
 
     # Display model summary
-    model.summary()
+    model.summary(print_fn=log.info)
 
     # Define the EarlyStopping callback
     early_stopping = EarlyStopping(
@@ -141,11 +152,28 @@ def main():
         validation_split=args.val_split,
         callbacks=callbacks,
     )
-    # TODO: Results should be saved to a file
 
-    # TODO: Spin off eval to a separate script
-    evaluate_model(model, x_test, y_test, target_tokenizer, source_tokenizer)
+    results = evaluate_model(model, x_test, y_test, target_tokenizer, source_tokenizer)
 
+    # Save results to a file
+    with open(os.path.join(results_folder_path, "results.json"), "w") as f:
+        json.dump(results, f, indent=4)
+
+    # Save args to a file
+    with open(os.path.join(results_folder_path, "args.json"), "w") as f:
+        json.dump(args.__dict__, f, indent=4)
+
+    # Save model
+    model.save(os.path.join(results_folder_path, "model.keras"))
+
+    # Save history
+    with open(os.path.join(results_folder_path, "history.json"), "w") as f:
+        json.dump(history.history, f, indent=4)
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    results_folder_name = f"{args.model}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4()}"
+    results_folder_path = os.path.join(args.results_dir, results_folder_name)
+    os.makedirs(results_folder_path, exist_ok=True)
+    log = setup_logger(results_folder_path, __name__)
+    main(args)
---
+++ src/utils.py
@@ -0,0 +1,27 @@
+import logging
+import os
+import sys
+
+def setup_logger(results_folder_path, name):
+    # Create a logger
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    # Create a file handler
+    file_handler = logging.FileHandler(os.path.join(results_folder_path, 'output.log'))
+    file_handler.setLevel(logging.INFO)
+
+    # Create a console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+
+    # Create a formatter
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    # Add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
