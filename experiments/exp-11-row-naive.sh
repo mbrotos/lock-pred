be928aebe4980c750479cf21765d03dbd7cddaca
Adam Sorrenti <adamsorrenti8@gmail.com>
2025-01-27 15:18:32 -0500

    naive baseline per table_name

---
+++ experiments/exp-11-row-naive.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# NOTE: This experiment is used to test naive baseline for each table separately.
+
+# SLURM configurations (will be ignored if not running on SLURM)
+#SBATCH --job-name=exp-11-row-locks-sep-naive
+#SBATCH --account=def-miranska
+#SBATCH --output=logs/exp-11-row-locks-sep-naive_%A_%a.out
+#SBATCH --error=logs/exp-11-row-locks-sep-naive_%A_%a.err
+#SBATCH --array=0-31 # NOTE: Make sure this is equal to the number of configs
+#SBATCH --time=2:59:00
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=adam.sorrenti@torontomu.ca
+#SBATCH --mail-type=ALL
+
+# Activate virtual environment
+source .venv/bin/activate
+
+ITERATIONS=1
+
+# Define experiment configurations
+declare -a configs_base=(
+    # Base configurations
+    "--experiment_name fixed/row_sep/exp-11-row-locks-naive/char_"
+)
+
+declare -a horizons=(1 2 3 4)
+
+declare -a tables=("DISTRICT" "STOCK" "ORDER_LINE" "ORDERS" "NEW_ORDER" "WAREHOUSE" "CUSTOMER" "HISTORY")
+
+# Generate configurations for each training data percentage
+declare -a configs
+for horizon in "${horizons[@]}"; do
+    for table in "${tables[@]}"; do
+        for config in "${configs_base[@]}"; do
+            configs+=("$config --horizon $horizon --val_split 0.0 --model transformer --remove_system_tables --token_length_seq --data data/fixed/row_sep/$table.csv --naive_baseline")
+        done
+    done
+done
+
+# Determine if running on SLURM or locally
+if [ -n "$SLURM_ARRAY_TASK_ID" ]; then
+    # Running on SLURM
+    task_id=$SLURM_ARRAY_TASK_ID
+    num_tasks=1
+else
+    # Running locally
+    task_id=0
+    num_tasks=${#configs[@]}
+fi
+
+# Main loop
+for ((i=task_id; i<task_id+num_tasks; i++)); do
+    current_config=${configs[$i]}
+    echo "Running experiment: $current_config"
+
+    # Run the experiment for the specified number of iterations
+    for j in $(seq 1 $ITERATIONS); do
+        echo "Running iteration $j"
+        python src/train.py $current_config
+    done
+done
