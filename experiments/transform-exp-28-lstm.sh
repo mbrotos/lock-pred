d52c4cdd1430cdb8af2375c35cd7f450ed3ac9ba
Adam Sorrenti <adamsorrenti8@gmail.com>
2025-03-25 06:05:20 -0400

    dedupe transform scripts

---
+++ experiments/transform-exp-28-lstm.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# SLURM configurations (will be ignored if not running on SLURM)
+#SBATCH --job-name=exp-28-lstm-dedupe
+#SBATCH --account=def-miranska
+#SBATCH --output=logs/exp-28-lstm-dedupe_%A_%a.out
+#SBATCH --error=logs/exp-28-lstm-dedupe_%A_%a.err
+#SBATCH --time=11:59:00
+#SBATCH --mem=128G
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=adam.sorrenti@torontomu.ca
+#SBATCH --mail-type=ALL
+#SBATCH --array=0-1
+module load gcc arrow
+module load python/3.11
+source ~/.venv/bin/activate
+
+set -e  # Exit immediately if a command exits with a non-zero status
+set -o pipefail  # Ensure pipeline errors are caught
+
+experiments=("exp-28-lstm-dedupe-row-locks" "exp-28-lstm-dedupe-table-locks")
+
+experiment=${experiments[$SLURM_ARRAY_TASK_ID]}
+echo "Extracting $experiment"
+
+python src/extract.py \
+    --experiment_name $experiment \
+    --output_file "results.csv" --iterations 10 # --skip_predictions 
+
+echo "Transforming $experiment"
+python src/transform.py \
+    --input_file "results/$experiment/results.csv" \
+    --output_file "results_clean.csv" --iterations 10
